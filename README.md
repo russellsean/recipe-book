a4587611a50c732eb829b1175f86cecf4868ed78
Sean Russell <srussell@dpndbl.com>
2019-02-01 21:26:04 -0600

    actually added vuetify

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Features:
 - Sign in feature Google or E&P
 - Users can create their own recipes and share them
 - Ability to edit and delete recipes (If owner)
+- Associate #tags to make recipes easy to find
 - Maybe other users can suggests recipe edits
---
+++ public/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <img height="500em" src="@/assets/banner.jpg" alt="banner">
+    <h1>This is the home page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+}
+</script>
